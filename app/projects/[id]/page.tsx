4e0da624d00fa54c64f91e5f5c5e09662eff8572
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2025-10-10 12:13:39 +0530

    fixed redirect

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -12,12 +12,8 @@ interface ProjectPageProps {
 export default async function ProjectPage({ params }: ProjectPageProps) {
   const { id } = await params;
 
-  return <Project projectID={id} />;
-}
-
- export async function generateMetadata({ params }: ProjectPageProps) {
-    const { id } = await params;
-    const apollo = await getServerApollo();
+  const apollo = await getServerApollo();
+  try {
     const { data } = await apollo.query<{
       project: {
         name: string;
@@ -32,14 +28,45 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
       `,
       variables: { id },
     });
-
-    const title = data?.project?.name || "Project";
-
     if (!data?.project) {
       redirect("/app");
     }
-
-    return {
-      title,
-    };
+  } catch {
+    redirect("/app");
   }
+
+  return <Project projectID={id} />;
+}
+
+export async function generateMetadata({ params }: ProjectPageProps) {
+  const { id } = await params;
+  const apollo = await getServerApollo();
+  let title = "Project";
+  try {
+    const { data } = await apollo.query<{
+      project: {
+        name: string;
+      };
+    }>({
+      query: gql`
+        query GetProject($id: ID!) {
+          project(id: $id) {
+            name
+          }
+        }
+      `,
+      variables: { id },
+    });
+    if (!data?.project) {
+      title = "project";
+    } else {
+      title = data.project.name || "Project";
+    }
+  } catch {
+    title = "Project";
+  }
+
+  return {
+    title,
+  };
+}
